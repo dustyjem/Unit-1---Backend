76c85ae4820c292ba6d3dd6d24970738afe872a7
dustyjem <97073289+dustyjem@users.noreply.github.com>
2023-10-17 16:25:45 -0600

    Images now display in the correct order

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -249,6 +249,8 @@ VALUES   (
   );
 
 
+-- Update file path for vehicle images
 UPDATE public.inventory
-SET inv_image = CONCAT(SUBSTRING(inv_image, 1, POSITION('/' IN inv_image)), '/vehicles', SUBSTRING(inv_image, POSITION('/' IN inv_image) + 1)),
-    inv_thumbnail = CONCAT(SUBSTRING(inv_thumbnail, 1, POSITION('/' IN inv_thumbnail)), '/vehicles', SUBSTRING(inv_thumbnail, POSITION('/' IN inv_thumbnail) + 1));
+
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+  inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
