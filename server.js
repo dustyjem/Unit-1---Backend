d8b2107aefba14f7ed94cd7e8a0aa29347f6f32b
dustyjem <97073289+dustyjem@users.noreply.github.com>
2023-10-17 15:32:00 -0600

    New update

--- server.js
+++ server.js
@@ -1,51 +1,91 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+
+ * This server.js file is the primary file of the
+
  * application. It is used to control the project.
+
  *******************************************/
+
 /* ***********************
+
  * Require Statements
+
  *************************/
+
 const express = require("express")
+
 const expressLayouts = require("express-ejs-layouts")
+
 const env = require("dotenv").config()
+
 const app = express()
+
 const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
+
 const inventoryRoute = require("./routes/inventoryRoute")
 
+const baseController = require("./controllers/baseController")
+
+ 
 
 /* ***********************
+
  * View Engine and Templates
+
  *************************/
+
 app.set("view engine", "ejs")
+
 app.use(expressLayouts)
-// app.use("/inv", inventoryRoute)
+
 app.set("layout", "./layouts/layout") // not at views root
-// app.get("/", (req, res) =>{
-//   res.render("index", {title: 'Home'}) 
-// })
+
+ 
 
 /* ***********************
+
  * Routes
+
  *************************/
+
 app.use(static)
+
+ 
+
+// Index route
+
 app.get("/", baseController.buildHome)
-app.use('inv', inventoryRoute)
-// app.use(express.static('public'));
-// app.get("/", baseController.buildHome)
+
+ 
+
 // Inventory routes
 
+app.use("/inv", inventoryRoute)
+
+ 
 
 /* ***********************
+
  * Local Server Information
+
  * Values from .env (environment) file
+
  *************************/
+
 const port = process.env.PORT
+
 const host = process.env.HOST
 
+ 
+
 /* ***********************
+
  * Log statement to confirm server operation
+
  *************************/
+
 app.listen(port, () => {
+
   console.log(`app listening on ${host}:${port}`)
+
 })
